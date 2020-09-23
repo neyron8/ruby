8674fd316643df85427e24319ea21f3495bfde72
neyron8 <dark.lords@mail.ru>
2020-09-23 18:04:17 +0700

    Test commit

--- lib/1practice.rb
+++ lib/1practice.rb
@@ -1,8 +1,19 @@
 class Converter
+  attr_accessor :temperature, :scale_f, :scale_s
+
   def set_temperature
     puts 'Введите температуру '
-    @temperature = gets
-    @temperature.chomp!
+    @temperature = gets.chomp
+  end
+
+  def set_first_scale
+    puts 'Введите название шкалы (С, K, F) '
+    @scale_f = gets.chomp!
+  end
+
+  def set_second_scale
+    puts 'Введите название шкалы в которую нужно перевести (С, K, F) '
+    @scale_s = gets.chomp!
   end
 
   def valid_scale?(scale)
@@ -17,18 +28,6 @@ class Converter
                    end
   end
 
-  def set_first_scale
-    puts 'Введите название шкалы (С, K, F) '
-    @scale_f = gets
-    @scale_f.chomp!
-  end
-
-  def set_second_scale
-    puts 'Введите название шкалы в которую нужно перевести (С, K, F) '
-    @scale_s = gets
-    @scale_s.chomp!
-  end
-
   def convert
     set_temperature
     abort 'Invalid temperature' unless valid_temperature?(@temperature)
@@ -36,53 +35,58 @@ class Converter
     abort "Invalid scale '#{@scale_f}' " unless valid_scale?(@scale_f)
     set_second_scale
     abort "Invalid scale '#{@scale_s}' " unless valid_scale?(@scale_s)
-    @temperature = transfToScale(@scale_f, @scale_s, @temperature)
+    @temperature = convert_to_scale(@scale_f)
     puts "Result is #{@temperature}"
   end
 
-  def transfToScale(scale_f, scale_s, temperature)
+  def cels_to_scale(scale_s)
+    case scale_s
+    when 'C'
+      @temperature
+    when 'F'
+      @temperature * 1.8 + 32
+    else
+      @temperature + 273.15
+    end
+  end
+
+  def fars_to_scale(scale_s)
+    case scale_s
+    when 'C'
+      (@temperature - 32) / 1.8
+    when 'F'
+      @temperature
+    else
+      (@temperature - 32) / 1.8 + 273.15
+    end
+  end
+
+  def kelvins_to_scale(scale_s)
+    case scale_s
+    when 'C'
+      @temperature - 273.15
+    when 'F'
+      1.8 * (@temperature - 273.15) + 32
+    else
+      @temperature
+    end
+  end
+
+  def convert_to_scale(scale_f)
     case scale_f
     when 'C'
-      case scale_s
-      when 'C'
-        # puts 'Cels into cels'
-        temperature += 0
-      when 'F'
-        # puts 'Cels into F'
-        temperature = temperature * 1.8 + 32
-      when 'K'
-        # puts 'Cels into K'
-        temperature += 273.15
-      end
+      cels_to_scale(@scale_s)
     when 'F'
-      case scale_s
-      when 'C'
-        # puts 'F into cels'
-        temperature = (temperature - 32) / 1.8
-      when 'F'
-        # puts 'F into F'
-        temperature += 0
-      when 'K'
-        # puts 'F into K'
-        temperature = (temperature - 32) / 1.8 + 273.15
-      end
-    when 'K'
-      case scale_s
-      when 'C'
-        # puts 'K into cels'
-        temperature -= 273.15
-      when 'F'
-        # puts 'K into F'
-        temperature = 1.8 * (temperature - 273.15) + 32
-      when 'K'
-        # puts 'K into K'
-        temperature += 0
-      end
+      fars_to_scale(@scale_s)
+    else
+      kelvins_to_scale(@scale_s)
     end
   end
 end
 
 obj = Converter.new
+# obj.temperature = 0.0
+# p obj.cels_to_scale('F')
 # puts obj.isValidTemperature?('12')
-# obj.convert
-puts obj.transfToScale('C', 'F', 50)
+obj.convert
+# puts obj.transfToScale('C', 'F', 50)
