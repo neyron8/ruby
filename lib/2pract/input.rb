51ccc57bd008749c4785ea27aa8ecabe5a7e7dbd
neyron8 <dark.lords@mail.ru>
2020-09-27 20:42:16 +0700

    Second lab without tests

--- lib/2pract/input.rb
+++ lib/2pract/input.rb
@@ -1,9 +1,9 @@
 # Input class
 class Input
-    def choose_operation
-      puts '1-Min, 2-Max, 3-Average, 4-Dispersion'
-      puts 'Type number of operation: '
-      @operation = gets.chomp!
-      @operation
-    end
+  def choose_operation
+    puts '1-Min, 2-Max, 3-Average, 4-Dispersion'
+    puts 'Type number of operation: '
+    @operation = gets.chomp!
+    @operation
+  end
 end
