be532a79c82cc7995d94572f06e3dca173d9c82c
neyron8 <dark.lords@mail.ru>
2020-09-27 21:23:33 +0700

    Second lab experimental test

--- spec/test2_spec.rb
+++ spec/test2_spec.rb
@@ -4,10 +4,16 @@ require 'csv'
 
 RSpec.describe Computing do
 	obj = Computing.new
-	describe '#Valid average' do
-		it "Checking 'ruby.csv' - true" do
+	describe '#Valid max' do
+		it "Max = 12.5 - true" do
 			# obj = Converter.new
 			expect(obj.comp_max('./lib/2pract/ruby.csv')).to eq(12.5)
+		end
+	end
+	describe '#Valid average' do
+		it "Max = 12.5 - true" do
+			# obj = Converter.new
+			expect(obj.comp_max('./lib/2pract/ruby.csv')).to eq(8.21)
 		end
 	end
 end
