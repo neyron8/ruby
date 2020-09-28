3a53cbad2e925eea2effcede2c6b02e569e332fd
neyron8 <dark.lords@mail.ru>
2020-09-28 13:18:52 +0700

    First and second lab(adding useful comments)

--- labs/lab1/spec/test1_spec.rb
+++ labs/lab1/spec/test1_spec.rb
@@ -9,7 +9,7 @@ RSpec.describe Converter do
       # obj = Converter.new
       expect(obj.valid_scale?('C')).to eq(true)
     end
-	it "Checking 'K' - true" do
+    it "Checking 'K' - true" do
       # obj = Converter.new
       expect(obj.valid_scale?('K')).to eq(true)
     end
@@ -27,15 +27,15 @@ RSpec.describe Converter do
       # obj = Converter.new
       expect(obj.valid_temperature?('q12')).to eq(false)
     end
-	it "Checking '10k' - false" do
+    it "Checking '10k' - false" do
       # obj = Converter.new
       expect(obj.valid_temperature?('10k')).to eq(false)
     end
-	it "Checking '934a8' - false" do
+    it "Checking '934a8' - false" do
       # obj = Converter.new
       expect(obj.valid_temperature?('934a8')).to eq(false)
     end
-	it "Checking '300' - true" do
+    it "Checking '300' - true" do
       # obj = Converter.new
       expect(obj.valid_temperature?('300')).to eq(true)
     end
@@ -58,39 +58,39 @@ RSpec.describe Converter do
     end
   end
   describe '#Valid convertation(object)' do
-	it '120 C to C' do
-	  # obj = Converter.new
-	  # obj.temperature = 12
-	  inp.temperature = 120;
-	  inp.scale_f = 'C'
-	  inp.scale_s = 'C'
-	  expect(obj.convert(inp)).to eq(120.0)
+    it '120 C to C' do
+      # obj = Converter.new
+      # obj.temperature = 12
+      inp.temperature = 120
+      inp.scale_f = 'C'
+      inp.scale_s = 'C'
+      expect(obj.convert(inp)).to eq(120.0)
     end
-	it '50 C to F' do
-	  # obj = Converter.new
-	  # obj.temperature = 12
-	  inp.temperature = 50;
-	  inp.scale_f = 'C'
-	  inp.scale_s = 'F'
-	  expect(obj.convert(inp)).to eq(122.0)
+    it '50 C to F' do
+      # obj = Converter.new
+      # obj.temperature = 12
+      inp.temperature = 50
+      inp.scale_f = 'C'
+      inp.scale_s = 'F'
+      expect(obj.convert(inp)).to eq(122.0)
     end
   end
   describe '#Valid convert to scale' do
-	it '12 K to C' do
-	  # obj = Converter.new
-	  # obj.temperature = 12
-	  inp.temperature = 12;
-	  inp.scale_f = 'K'
-	  inp.scale_s = 'C'
-	  expect(obj.convert(inp)).to eq(-261.15)
+    it '12 K to C' do
+      # obj = Converter.new
+      # obj.temperature = 12
+      inp.temperature = 12
+      inp.scale_f = 'K'
+      inp.scale_s = 'C'
+      expect(obj.convert(inp)).to eq(-261.15)
     end
-	it '50 C to F' do
-	  # obj = Converter.new
-	  # obj.temperature = 12
-	  inp.temperature = 50;
-	  inp.scale_f = 'C'
-	  inp.scale_s = 'F'
-	  expect(obj.convert(inp)).to eq(122.0)
+    it '50 C to F' do
+      # obj = Converter.new
+      # obj.temperature = 12
+      inp.temperature = 50
+      inp.scale_f = 'C'
+      inp.scale_s = 'F'
+      expect(obj.convert(inp)).to eq(122.0)
     end
   end
 end
