41952b891be1489cde1d809c13fbe8da314cb00a
neyron8 <dark.lords@mail.ru>
2020-09-28 14:02:54 +0700

    Second lab reworked

--- labs/lab2/lib/computing.rb
+++ labs/lab2/lib/computing.rb
@@ -15,10 +15,10 @@ class Computing
   end
 
   def comp_min
-    table = CSV.read('/labs/lab2/lib/ruby.csv', col_sep: ';', headers: true, converters: :numeric)
+    table = CSV.read('labs/lab2/lib/ruby.csv', col_sep: ';', headers: true, converters: :numeric)
     @min = table[0][1]
 
-    CSV.foreach('./ruby.csv', col_sep: ';', headers: true, converters: :numeric) do |(_x, y)|
+    CSV.foreach('labs/lab2/lib/ruby.csv', col_sep: ';', headers: true, converters: :numeric) do |(_x, y)|
       @min = y[1] if @min > y[1]
     end
     @min.round(2)
@@ -27,7 +27,7 @@ class Computing
   def comp_average
     @sum = 0
     @n = 0
-    CSV.foreach('/labs/lab2/lib/ruby.csv', col_sep: ';', headers: true, converters: :numeric) do |(_x, y)|
+    CSV.foreach('labs/lab2/lib/ruby.csv', col_sep: ';', headers: true, converters: :numeric) do |(_x, y)|
       @sum += y[1]
       @n += 1
     end
@@ -37,7 +37,7 @@ class Computing
   def comp_dispers
     @average = comp_average
     @n = 0
-    CSV.foreach('/labs/lab2/lib/ruby.csv', col_sep: ';', headers: true, converters: :numeric) do |(_x, y)|
+    CSV.foreach('labs/lab2/lib/ruby.csv', col_sep: ';', headers: true, converters: :numeric) do |(_x, y)|
       @sum += (y[1] - @average)**2
       @n += 1
     end
