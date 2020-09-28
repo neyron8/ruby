eb8599033fba0ee15712c7619f65fcd55a0cf64c
neyron8 <dark.lords@mail.ru>
2020-09-28 13:41:22 +0700

    Second lab

---
+++ labs/lab2/lib/calculate.rb
@@ -0,0 +1,14 @@
+load 'input.rb'
+load 'computing.rb'
+
+# Final class
+class Calculate
+  def calculate
+    obj = Input.new
+    comp = Computing.new
+    comp.computing_operation(obj.choose_operation, 'ruby.csv')
+  end
+end
+
+obj = Calculate.new
+p obj.calculate
