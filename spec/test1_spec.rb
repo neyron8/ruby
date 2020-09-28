176ef2ce260e71a17115b8ae87dd2f212474837f
neyron8 <dark.lords@mail.ru>
2020-09-28 12:44:03 +0700

    First and second lab(adding useful comments)

--- spec/test1_spec.rb
+++ spec/test1_spec.rb
@@ -9,6 +9,10 @@ RSpec.describe Converter do
       # obj = Converter.new
       expect(obj.valid_scale?('C')).to eq(true)
     end
+	it "Checking 'K' - true" do
+      # obj = Converter.new
+      expect(obj.valid_scale?('K')).to eq(true)
+    end
     it "Checking 'Q' - false" do
       # obj = Converter.new
       expect(obj.valid_scale?('Q')).to eq(false)
