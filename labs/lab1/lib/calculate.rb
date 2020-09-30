39a1fd79cfd7d9184fba3a154e5b9f5fdb186185
neyron8 <dark.lords@mail.ru>
2020-09-30 10:09:58 +0700

    Second lab removed some tests

--- labs/lab1/lib/calculate.rb
+++ labs/lab1/lib/calculate.rb
@@ -7,10 +7,10 @@ load 'converter.rb'
 class Calculate
   def self.calculate
     temperature = Input.set_temperature
-    scale_f = Input.set_scale
-    scale_s = Input.set_scale
     abort 'Invalid temperature' unless Converter.valid_temperature?(temperature)
+    scale_f = Input.set_scale
     abort "Invalid scale '#{scale_f}' " unless Converter.valid_scale?(scale_f)
+    scale_s = Input.set_scale
     abort "Invalid scale '#{scale_s}' " unless Converter.valid_scale?(scale_s)
     Converter.convert_to_scale(temperature, scale_f, scale_s)
   end
