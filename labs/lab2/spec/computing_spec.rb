72212ff749b7af4bb683fe9bf70d0bcf9ca028e4
neyron8 <dark.lords@mail.ru>
2020-09-30 09:36:52 +0700

    Second lab removed some tests

--- labs/lab2/spec/computing_spec.rb
+++ labs/lab2/spec/computing_spec.rb
@@ -2,39 +2,41 @@ load 'computing.rb'
 require 'csv'
 
 RSpec.describe Computing do
-  it 'Valid operation 1' do
-    expect(Computing.valid_operation?('1')).to eq true
-  end
+  describe '.computing' do
+    it 'Valid operation 1' do
+      expect(Computing.valid_operation?('1')).to eq true
+    end
 
-  it 'Valid operation 2' do
-    expect(Computing.valid_operation?('2')).to eq true
-  end
+    it 'Valid operation 2' do
+      expect(Computing.valid_operation?('2')).to eq true
+    end
 
-  it 'Valid operation 3' do
-    expect(Computing.valid_operation?('3')).to eq true
-  end
+    it 'Valid operation 3' do
+      expect(Computing.valid_operation?('3')).to eq true
+    end
 
-  it 'Valid operation 4' do
-    expect(Computing.valid_operation?('4')).to eq true
-  end
+    it 'Valid operation 4' do
+      expect(Computing.valid_operation?('4')).to eq true
+    end
 
-  it 'Unvalid operation 5' do
-    expect(Computing.valid_operation?('5')).to eq false
-  end
+    it 'Unvalid operation 5' do
+      expect(Computing.valid_operation?('5')).to eq false
+    end
 
-  it 'Computing min' do
-    expect(Computing.computing_operation('1')).to eq 6.1
-  end
+    it 'Computing min' do
+      expect(Computing.computing_operation('1')).to eq 6.1
+    end
 
-  it 'Computing max' do
-    expect(Computing.computing_operation('2')).to eq 12.5
-  end
+    it 'Computing max' do
+      expect(Computing.computing_operation('2')).to eq 12.5
+    end
 
-  it 'Computing average' do
-    expect(Computing.computing_operation('3')).to eq 8.23
-  end
+    it 'Computing average' do
+      expect(Computing.computing_operation('3')).to eq 8.23
+    end
 
-  it 'Computing dispers' do
-    expect(Computing.computing_operation('4')).to eq 2.98
+    it 'Computing dispers' do
+      expect(Computing.computing_operation('4')).to eq 2.98
+    end
   end
 end
