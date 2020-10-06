8d47bc7485f4b1e7fb54d18eea964a8d1bafb0a8
neyron8 <dark.lords@mail.ru>
2020-10-06 15:56:58 +0700

    Changed 2 lab tests

--- labs/lab2/spec/computing_spec.rb
+++ labs/lab2/spec/computing_spec.rb
@@ -2,47 +2,47 @@ load 'computing.rb'
 require 'csv'
 
 RSpec.describe Computing do
-  obj do
+  subject do
     Computing.new
   end
 
   describe '#valid_operation?' do
     context 'Input value - 1, number in range' do
-      it { expect(obj.valid_operation?('1')).to eq true }
+      it { expect(subject.valid_operation?('1')).to eq true }
     end
 
     context 'Input value - 2, number in range' do
-      it { expect(obj.valid_operation?('2')).to eq true }
+      it { expect(subject.valid_operation?('2')).to eq true }
     end
 
     context 'Input value - 3, number in range' do
-      it { expect(obj.valid_operation?('3')).to eq true }
+      it { expect(subject.valid_operation?('3')).to eq true }
     end
 
     context 'Input value - 4, number in range' do
-      it { expect(obj.valid_operation?('4')).to eq true }
+      it { expect(subject.valid_operation?('4')).to eq true }
     end
 
     context 'Input value - 5, number not in range' do
-      it { expect(obj.valid_operation?('5')).to eq false }
+      it { expect(subject.valid_operation?('5')).to eq false }
     end
   end
 
   describe '#computing_operation' do
     context 'Computing min' do
-      it { expect(obj.computing_operation('1')).to eq 6.1 }
+      it { expect(subject.computing_operation('1')).to eq 6.1 }
     end
 
     context 'Computing max' do
-      it { expect(obj.computing_operation('2')).to eq 12.5 }
+      it { expect(subject.computing_operation('2')).to eq 12.5 }
     end
 
     context 'Computing average' do
-      it { expect(obj.computing_operation('3')).to eq 8.23 }
+      it { expect(subject.computing_operation('3')).to eq 8.23 }
     end
 
     context 'Computing dispers' do
-      it { expect(obj.computing_operation('4')).to eq 2.65 }
+      it { expect(subject.computing_operation('4')).to eq 2.65 }
     end
   end
 end
