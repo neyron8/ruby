e85017daca673a70ae154eda73e2747128ecf1bb
neyron8 <32508305+neyron8@users.noreply.github.com>
2020-10-26 14:57:19 +0700

    Update converter_spec.rb

--- labs/lab1/spec/converter_spec.rb
+++ labs/lab1/spec/converter_spec.rb
@@ -2,63 +2,63 @@ require './lib/converter'
 
 RSpec.describe Converter do
   describe '#valid_scale?' do
-    context 'Input value - C, true type' do
+    context 'Input value - C true' do
       it { expect(Converter.valid_scale?('C')).to eq true }
     end
 
-    context 'Input value - K, true type' do
+    context 'Input value - K true' do
       it { expect(Converter.valid_scale?('K')).to eq true }
     end
 
-    context 'Input value - F, true type' do
+    context 'Input value - F true' do
       it { expect(Converter.valid_scale?('F')).to eq true }
     end
 
-    context 'Input value - Q, false type' do
+    context 'Input value - Q false' do
       it { expect(Converter.valid_scale?('Q')).to eq false }
     end
   end
 
   describe '#valid_temperature?' do
-    context 'Input temperature is valid' do
+    context 'Input temperature true' do
       it { expect(Converter.valid_temperature?('120')).to eq true }
     end
 
-    context 'Input temperature is not valid' do
+    context 'Input temperature false' do
       it { expect(Converter.valid_temperature?('q12')).to eq false }
     end
   end
 
   describe '#convert_to_scale' do
-    context '120 C conv to C' do
+    context 'C to C' do
       it { expect(Converter.convert_to_scale(120, 'C', 'C')).to eq 120.0 }
     end
 
-    context '50 C conv to F' do
+    context 'C to F' do
       it { expect(Converter.convert_to_scale(50, 'C', 'F')).to eq 122.0 }
     end
 
-    context '40 C conv to K' do
+    context 'C to K' do
       it { expect(Converter.convert_to_scale(40, 'C', 'K')).to eq 313.15 }
     end
 
-    context '14 F conv to K' do
+    context 'F to K' do
       it { expect(Converter.convert_to_scale(14, 'F', 'K')).to eq 263.15 }
     end
 
-    context '14 F conv to F' do
+    context 'F to F' do
       it { expect(Converter.convert_to_scale(14, 'F', 'F')).to eq 14.0 }
     end
 
-    context '14 K conv to C' do
+    context 'K to C' do
       it { expect(Converter.convert_to_scale(14, 'K', 'C')).to eq(-259.15) }
     end
 
-    context '568 K conv to F' do
+    context 'K to F' do
       it { expect(Converter.convert_to_scale(568, 'K', 'F')).to eq 562.73 }
     end
 
-    context '568 K conv to K' do
+    context 'K to K' do
       it { expect(Converter.convert_to_scale(568, 'K', 'K')).to eq(568.0) }
     end
   end
